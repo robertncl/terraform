e799b269b062e8b818d06f597ff69b8e5e58f11d
Robert Ngo <robertncl@gmail.com>
2025-01-01 16:00:41 +0800

    add open ai

--- main.tf
+++ main.tf
@@ -49,33 +49,13 @@ resource "azurerm_subnet" "test" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_log_analytics_workspace" "test" {
-  name                = "acctest-2323"
+resource "azurerm_ai_services" "test" {
+  name                = "example-account"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
+  sku_name            = "S0"
 
-resource "azurerm_container_app_environment" "test" {
-  name                       = "Example-Environment"
-  location                   = azurerm_resource_group.test.location
-  resource_group_name        = azurerm_resource_group.test.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.test.id
-}
-
-resource "azurerm_container_app" "test" {
-  name                         = "example-app"
-  container_app_environment_id = azurerm_container_app_environment.test.id
-  resource_group_name          = azurerm_resource_group.test.name
-  revision_mode                = "Single"
-
-  template {
-    container {
-      name   = "examplecontainerapp"
-      image  = "mcr.microsoft.com/k8se/quickstart:latest"
-      cpu    = 0.25
-      memory = "0.5Gi"
-    }
+  tags = {
+    Acceptance = "Test"
   }
 }
