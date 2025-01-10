f94202f4993edd4c79ad16dbf9286f6518061e82
Robert Ngo <robertncl@gmail.com>
2025-01-10 10:12:04 +0800

    add aks

--- main.tf
+++ main.tf
@@ -49,13 +49,27 @@ resource "azurerm_subnet" "test" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_ai_services" "test" {
-  name                = "example-account"
-  location            = "East US"
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "exampleaks"
+  location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  sku_name            = "S0"
+  dns_prefix          = "exampleaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
 
   tags = {
-    Acceptance = "Test"
+    environment = "testing"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,11 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
+
+// Add outputs for AKS cluster
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+}
